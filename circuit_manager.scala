8e0418d5d6fb11b2d90bc49dd3f0239a025fa198
PARK HYEONJAE <p0611sp2@naver.com>
2018-05-06 20:37:06 +0900

    first commit

---
+++ circuit_manager.scala
@@ -0,0 +1,43 @@
+package circuit.manager
+
+import chisel3._
+import circuit.matrix._
+
+class RecordController extends Module {
+	val io = IO(new Bundle {
+		val command = Input()
+		val jobEnd = Input(Bool())
+		val go = Output(Bool())
+		val toDownRecord = Output()
+		val toRightRecord = Output()
+	})
+	// m * n Matrix? Records
+	val downRecord =
+		Vec(Seq.fill(m){ Module(new RECORD()).io })
+	val rightRecord =
+		Vec(Seq.fill(n){ Module(new RECORD()).io })
+	// receive command
+	when(?) {
+		when(?) {
+		} .elsewhen(?)
+	}
+}
+
+class Record extends Module {
+	val io = IO(new Bundle {
+		val get = Input(UInt(8.W))
+		val toCell = Output(UInt(8.W))
+		val toNext = Output(UInt(8.W))
+	})
+	// Initialize
+	when(reset.toBool) {
+		r0 := 0.U
+		r1 := 0.U
+	}
+	// Keep Value
+	val r0 = RegNext(io.get)
+	val r1 = RegNext(r0)
+	// Output
+	io.toCell := r0
+	io.toNext := r1
+}
