a99c561b2b8148f83e7910b57fde8bc959320595
Poment <p0611sp2@naver.com>
2018-06-18 15:37:06 +0900

    06.18 update

--- src/main/scala/DolphiN/SystolicArray.scala
+++ src/main/scala/DolphiN/SystolicArray.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 
 /* CORE CIRCUIT */
-class RecordController(m: Int, n: Int) extends Module {
+class RecordController(s: Int) extends Module {
 	val io = IO(new Bundle {
 		val begin = Input(Bool())
 		val a = Input(UInt(8.W))
@@ -13,72 +13,72 @@ class RecordController(m: Int, n: Int) extends Module {
 		val finish = Output(Bool())
 	})
 	// Register Setting ===================================================
-	val timer = Reginit(2.U(16.W))
-	// Modules =========================================================
-	// >>>> Queue setting
-	val Mrecords =
-		Vec(Seq.fill(m){ Module(new Record(m+n)).io })
-	val Nrecords =
-		Vec(Seq.fill(n){ Module(new Record(n+m)).io })
+	val timer = RegInit(255.U(16.W))
 	val counter = RegInit(0.U(8.W))
-	// >>>> Element setting
-	val Elements = Vec(Seq.fill(m * n){ Module(new Element()).io })
-	// >>>> Accumulator setting
-	val TheStorage = Module(new Storage(m, n))
+	// Modules =========================================================
+	// >>>> Queues setting
+	val Mrecords = Vec(Seq.fill(s){ Module(new Record(s)).io })
+	val Nrecords = Vec(Seq.fill(s){ Module(new Record(s)).io })
+	// >>>> Elements setting
+	val Elements = Vec(Seq.fill(s*s){ Module(new Element()).io })
+	// >>>> Storage setting
+	val TheStorage = Module(new Storage(s))
 	// IO ==============================================================
 	// >>>> Elements - Storages
-	for(i <- 0 until m * n) {
+	for(i <- 0 until s*s) {
 		TheStorage.io.fromElements(i) := Elements(i).toAccumulator
 	}
-
 	// >>>> Element - Element
 	// >> Left - Right
-	for(j <- 0 until m) {
-		for(i <- 0 until n-1) {
-			Elements(i+1+(j*n)).fromLeft := Elements(i+(j*n)).toRight
+	for(j <- 0 until s) {
+		for(i <- 0 until s-1) {
+			Elements(i+1+(j*s)).fromLeft := Elements(i+(j*s)).toRight
 		}
 	}
 	// >> Top - Down
-	for(j <- 0 until m-1) {
-		for(i <- 0 until n) {
-			Elements(((j+1)*n)+i).fromTop := Elements((j*n)+i).toDown
+	for(j <- 0 until s-1) {
+		for(i <- 0 until s) {
+			Elements(((j+1)*s)+i).fromTop := Elements((j*s)+i).toDown
 		}
 	}
 	// >>>> Queue - Element
-	for(i <- 0 until m) {
-		Elements(i*n).fromLeft := Mrecords(i).toElement
+	for(i <- 0 until s) {
+		Elements(i*s).fromLeft := Mrecords(i).toElement
 	}
-	for(i <- 0 until n) {
+	for(i <- 0 until s) {
 		 Elements(i).fromTop := Nrecords(i).toElement
 	}
-	// Controlling
-	// >>>> timer control & Queue control
-	when(io.begin) {
+	// Controlling ========================================================
+	// >>>> timer control & queue control
+	// io.begin : true
+	when( io.begin ) {
 		timer := timer - 1.U
 		Mrecords(counter).begin := true.B
 		Nrecords(counter).begin := true.B
 		counter := counter + 1.U
 	}
+	// io.begin : false
 	.otherwise {
 		timer := io.a + io.k + io.b - 1.U
-		for(i <- 0 until m) {
+		for(i <- 0 until s) {
 			Mrecords(i).begin := false.B
-		}
-		for(i <- 0 until n) {
 			Nrecords(i).begin := false.B
 		}
 		counter := 0.U
 	}
 	// >>>> reset control
 	TheStorage.io.reset := true.B
+	io.finish := false.B
+	// timer == 0
 	when(timer === 0.U) {
 		TheStorage.io.reset := true.B
-		io.finish = true.B
-		timer := 2.U
+		io.finish := true.B
+		timer := 255.U
 	}
+	// timer != 0
 	when(timer =/= 0.U) {
 		TheStorage.io.reset := false.B
-		io.finish = false.B
+		io.finish := false.B
 	}
 }
 
@@ -91,26 +91,25 @@ class Record(x: Int) extends Module {
 		val toElement = Output(UInt(8.W))
 	})
 	// Initialize
-	val buffers =  Reg(init = Vec(Seq.fill( x )( 0.U(8.W) )))
+	val buffers = Reg(init = Vec(Seq.fill(x)(0.U(8.W))))
 	// Controlling
-	// >>>> when io.get is enable
+	// io.get : true
 	when( io.get ) {
 		for (i <- x-1 to 1 by -1) {
-    			buffers(i) := buffers(i - 1)
+    			buffers(i) := buffers(i-1)
   		}
   		buffers(0) := io.fromController
 		io.toElement := 0.U
-  		
 	}
-	// >>>> when io.get is unable and io.begin is enable
+	// io.get : false && io.begin : true
 	.elsewhen( io.begin ) {
-			for (i <- x-1 to 1 by -1) {
-    				buffers(i) := buffers(i - 1)
-  			}
-			io.toElement := buffers(x-1)
+		for (i <- x-1 to 1 by -1) {
+    			buffers(i) := buffers(i-1)
   		}
+		io.toElement := buffers(x-1)
+		buffers(0) := 0.U
 	}
-	// >>>> when not get, not begin
+	// io.get : false && io.begin : false
 	.otherwise {
 		for (i <- 0 until x) {
     			buffers(i) := buffers(i)
@@ -120,6 +119,7 @@ class Record(x: Int) extends Module {
 }
 
 /* Matrix mul result Element */
+/* Complete !!!!!            */
 class Element extends Module {
 	val io = IO(new Bundle {
 		val fromLeft = Input(UInt(8.W))
@@ -143,27 +143,26 @@ class Element extends Module {
 }
 
 /* Result storage */
-class Storage(m: Int, n: Int) extends Module {
+class Storage(x: Int) extends Module {
 	val io = IO(new Bundle {
 		val reset = Input(Bool())
-		val fromElements = Input(Vec( m * n, UInt(8.W) ))
-		val toController = Output(UInt(( 32 * m * n ).W))
+		val fromElements = Input(Vec(x*x, UInt(8.W)))
+		val toController = Output(UInt((32*x*x).W))
 	})
-	// accumulator
-	val accumulator = RegInit(Vec(Seq.fill( m * n )( 0.U(32.W) )))
-	val total = UInt(( 32 * m * n ).W)
-
+	// Accumulators setting
+	val accumulator = RegInit(Vec(Seq.fill(x*x)(0.U(32.W))))
 	// match element and storage
-	for(i <- 0 until (m * n)) {
-		accumulator(i) := accumulator(i) + io.fromElements(i)
-	}
+	// io.reset : true -> reset
 	when(io.reset) {
-		for(i <- 0 until (m * n)) {
+		for(i <- 0 until (x*x)) {
 			accumulator(i) := 0.U
 		}
 	}
-	// Concat
-	//for(i <- 0 until m * n) {
-	//	total := Cat(accumulator(i), accumulator(i+1))
-	//}
+	// io.rest : false -> get from elements
+	.otherwise {
+		for(i <- 0 until (x*x)) {
+			accumulator(i) := accumulator(i) + io.fromElements(i)
+		}
+	}
+	// how concat
 }
