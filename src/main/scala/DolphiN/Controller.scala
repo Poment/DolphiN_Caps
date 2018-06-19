8cb20d884d3943fb3885b95204bd6ca2a4fa4347
minmossi <alswg1994@gmail.com>
2018-06-19 19:30:45 +0900

    Update

--- src/main/scala/DolphiN/Controller.scala
+++ src/main/scala/DolphiN/Controller.scala
@@ -9,17 +9,23 @@ class Controller(implicit p: Parameters) extends LazyRoCC {
 }
 
 // HasCoreParameteres? HasL1CacheParameters? Parameters? other parameters?
-class ControllerModule(outer: Controller) extends LazyRoCCModule(outer) {
+class ControllerModule(outer: Controller, m: Int) extends LazyRoCCModule(outer) {
+  val systolicArray = new RecordController(m)
+
   val cmd = Queue(io.cmd)
   val funct = cmd.bits.inst.funct
-  val doReport = funct === UInt(1)
-  val doReserve = funct === UInt(2)
-  val doSetParam = funct === UInt(4)
-  val doSetSpParam = funct === UInt(8)
-  val doStart = funct === UInt(16)
-  val doReset = funct === UInt(32)
+  val doReport = funct === UInt(0)
+  val doReserve = funct === UInt(1)
+  val doSetParam = funct === UInt(2)
+  val doStart = funct === UInt(3)
+  val doRelease = funct === UInt(4)
 
   // These below has to be modified in width for scalability?
+  val regReserve = Reg(init = Uint(0))
+  val regStart = Reg(init = Uint(0))
+  val regLoad = Reg(init = Uint(0))
+  val regAddr = Reg(UInt(width = 64))
+  val regOffset = Reg(init = UInt(0, width = 16))
   val regA = Reg(UInt(width = 8))
   val regB = Reg(UInt(width = 8))
   val regK = Reg(UInt(width = 8))
@@ -36,33 +42,52 @@ class ControllerModule(outer: Controller) extends LazyRoCCModule(outer) {
   val regN = Reg(UInt(width = 8))
 
   when (cmd.fire() && doReport){
+    when (regRes){
 
+    }
+    .elsewhen (regRes){
+
+    }
+    .otherwise{
+
+    }
   }
-  when (cmd.fire() && doReserve){
+  .elsewhen (cmd.fire() && doReserve){
+    regReserve := UInt(1)
     regOA := io.cmd.bits.rs1
     regMA := io.cmd.bits.rs2
-
   }
-  when (cmd.fire() && doSetParam){
-    regA := 
-    regB := 
-    regK := 
-    regXO0 := 
-    regXO1 := 
-    regYO0 := 
-    regYO1 := 
-
+  .elsewhen (cmd.fire() && doSetParam){
+    regA := io.cmd.bits.rs1(15,8)
+    regB := io.cmd.bits.rs1(23,16)
+    regK := io.cmd.bits.rs1(31,24)
+    regXO0 := io.cmd.bits.rs2(7,0)
+    regXO1 := io.cmd.bits.rs2(15,8)
+    regYO0 := io.cmd.bits.rs2(23,15)
+    regYO1 := io.cmd.bits.rs2(31,24)
   }
-  when (cmd.fire() && doSetSpParam){
-    regBA := 
-
+  .elsewhen (cmd.fire() && doStart){
+    regStart := UInt(1)
+    regAddr := regIA0 + regOffset
+    io.mem.req.valid := cmd.valid && doLoad && !stallReg && !stallResp
+    io.mem.req.bits.addr := regAddr
+    io.mem.req.bits.tag := 
+    io.mem.req.bits.cmd := M_XRD
+    io.mem.req.bits.typ := MT_D
+    io.mem.req.bits.data := Bits(0)
+    io.mem.req.bits.phys := Bool(false)
   }
-  when (cmd.fire() && doStart){
-    regIA0 := 
-    regIA1 := 
-
+  .elsewhen (regStart){
+    regLoad = UInt(1)
+    io.mem.req.bits.addr := regIA0
+    reg = regIA0 + 8
   }
-  when (cmd.fire() && doReset){
-
+  .elsewhen (regLoad){
+    regIA0 = regIA0 + 1
+  }
+  .elsewhen (cmd.fire() && doRelease){
+    regReserve := UInt(0)
+    regStart := UInt(0)
+    regLoad := UInt(0)
   }
 }
